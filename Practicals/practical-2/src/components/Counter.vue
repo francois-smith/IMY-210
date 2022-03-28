1bf1a44657170a759067c8177333db53a2a91029
Francois Smith <89078211+francois-smith@users.noreply.github.com>
2022-03-28 20:00:31 +0200

    Practical 2 Finished Componet Based Like System

---
+++ Practicals/practical-2/src/components/Counter.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1 v-bind:class="{liked: isActive}">{{value}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CounterComponent',
+    props: {
+        value: Number,
+        isActive: Boolean
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;1,100;1,200;1,300&display=swap');
+
+    h1{
+        font-family: Montserrat, sans-serif;
+        font-size: 40px;
+        font-weight: 400;
+        color: #ed4d4d;
+        margin: 0px;
+    }
+
+    .liked{
+        color: #63b34f;
+    }
+</style>
