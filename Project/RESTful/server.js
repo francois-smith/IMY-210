c6e9f25e657beedbc735e57a06a0be8d241fe2df
Francois Smith <steamfaffa@gmail.com>
2022-05-14 19:42:52 +0200

    Added Server Basics

---
+++ Project/RESTful/server.js
@@ -0,0 +1,13 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+let fs = require('fs');
+
+let app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+let routes = require('./router/router.js')(app, fs);
+
+let port  = process.env.PORT || 3000;
+app.listen(port);
+console.log('Server is running on port ' + port);
