0a5208e57e57cbec3824559541d9c014cf316249
Francois Smith <steamfaffa@gmail.com>
2022-05-21 23:34:14 +0200

    Redid Styling And Removed Function

--- Project/Vue/src/components/SheduleMonthSelector.vue
+++ Project/Vue/src/components/SheduleMonthSelector.vue
@@ -1,8 +1,11 @@
 <template>
 	<div class="calendar-date-selector">
-		<span @click="selectPrevious">﹤</span>
-		<span @click="selectCurrent">Today</span>
-		<span @click="selectNext">﹥</span>
+		<img class="button-left" @click="selectPrevious" src="../media/angle.svg">
+		<div class="current-month">
+			<img src="../media/calendar.svg">
+			<span>{{ selectedMonth }}</span>
+		</div>
+		<img class="button-right" @click="selectNext" src="../media/angle.svg">
   </div>
 </template>
 
@@ -12,14 +15,14 @@ import dayjs from "dayjs";
 export default {
 	name: 'ScheduleMonthSelector',
 	props: {
-		currentDate: {
-			type: String,
-			required: true
-		},
-
 		selectedDate: {
 			type: Object,
 			required: true
+		}
+	},
+	computed: {
+		selectedMonth() {
+			return this.selectedDate.format("MMMM YYYY");
 		}
 	},
 	methods: {
@@ -27,12 +30,6 @@ export default {
 			let newSelectedDate = dayjs(this.selectedDate).subtract(1, "month");
 			this.$emit("dateSelected", newSelectedDate);
 		},
-
-		selectCurrent() {
-			let newSelectedDate = dayjs(this.currentDate);
-			this.$emit("dateSelected", newSelectedDate);
-		},
-
 		selectNext() {
 			let newSelectedDate = dayjs(this.selectedDate).add(1, "month");
 			this.$emit("dateSelected", newSelectedDate);
@@ -42,15 +39,48 @@ export default {
 </script>
 
 <style scoped>
-.calendar-date-selector {
-  display: flex;
-  justify-content: space-between;
-  width: 80px;
-  color: var(--grey-800);
+.button-right{
+	border: 1px solid #e4e4e49d !important;
+	width: 11px;
+	padding: 8px 12px;
+	transform: scaleX(-1);
+	cursor: pointer;
+	user-select: none;
+	border-radius: 6px 0px 0px 6px;
 }
 
-.calendar-date-selector > * {
-  cursor: pointer;
-  user-select: none;
+.button-right:hover, .button-left:hover{
+	background-color: rgb(226, 226, 226);
+}
+
+.button-left{
+	border: 1px solid #e4e4e49d !important;
+	padding: 8px 12px;
+	width: 11px;
+	cursor: pointer;
+	user-select: none;
+	border-radius: 6px 0px 0px 6px;
+}
+
+.current-month{
+	font-size: 18px;
+	display: flex;
+	padding: 8px 12px;
+	align-items: center;
+	border: 1px solid #e4e4e49d !important;
+	margin: 0px -1px;
+}
+.current-month span{
+	padding-left: 10px;
+	font-weight: 500;
+}
+
+.current-month img{
+	width: 25px;
+}
+
+.calendar-date-selector {
+	display: flex;
+	color: rgb(65, 65, 65);
 }
 </style>
